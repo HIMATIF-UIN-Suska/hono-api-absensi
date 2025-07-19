59752ce86f1dd7d0f762bcdce64b96cfd6e1adeb
Asddqphn1 <assiddiqiphn@gmail.com>
2025-07-19 16:38:11 +0700

    feat: update kartu-rfid route to use KartuRfidHandler for handling requests

--- src/routes/kartu-rfid.route.ts
+++ src/routes/kartu-rfid.route.ts
@@ -2,12 +2,17 @@ import { Hono } from "hono";
 import { RegExpRouter } from "hono/router/reg-exp-router";
 import { zodError } from "../utils/zod-error.util";
 import { zValidator } from "@hono/zod-validator";
-import MahasiswaHandler from "../handlers/kartu-rfid.handler";
-import { postRfidSchema } from "../validators/rfid.validator";
+import { postRfidSchema, rfidIdSchema } from "../validators/rfid.validator";
+import KartuRfidHandler from "../handlers/kartu-rfid.handler";
 
 
 const kartuRfidRoute = new Hono({ router: new RegExpRouter() });
 
-kartuRfidRoute.post("/kartu-rfid", zValidator("json", postRfidSchema, zodError), MahasiswaHandler.post);
+kartuRfidRoute.post("/kartu-rfid", zValidator("json", postRfidSchema, zodError), KartuRfidHandler.post);
+kartuRfidRoute.get("/aktivasi-kartu/:id", KartuRfidHandler.verify);
+kartuRfidRoute.get("/rfid/:id", KartuRfidHandler.get);
+kartuRfidRoute.get("/daftar-absensi", KartuRfidHandler.getAbsensi);
+kartuRfidRoute.post("/rfid/absensi", zValidator("json", rfidIdSchema, zodError), KartuRfidHandler.absensi);
+
 
 export default kartuRfidRoute;
