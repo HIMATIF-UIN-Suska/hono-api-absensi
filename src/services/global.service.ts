1bc618f19c03ed8068c2cfcf3f11e4bb33dcabe4
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-09-04 23:17:51 +0700

    🔥feat: update welcome messages and contributor link in GlobalService

--- src/services/global.service.ts
+++ src/services/global.service.ts
@@ -5,9 +5,9 @@ export default class GlobalService {
     public static async introduce(): Promise<GlobalServiceIntroduceResponse> {
         return {
             'response': true,
-            'message': 'Cihuy, Halow Semua 👋 ~ Selamat datang di API Kerja Praktik! 🎉',
+            'message': 'Cihuy, Halow Semua 👋 ~ Selamat datang di API Absensi HIMATIF UIN Suska Riau! 🎉',
             'version': process.env.APP_VERSION || '1.0.0',
-            'contributor': 'https://github.com/MFarhanZ1/hono-api-kerja-praktik',
+            'contributor': 'https://github.com/HIMATIF-UIN-Suska/hono-api-absensi',
             'timezone': `Asia/Jakarta ~ ${new Date().toLocaleString('id-ID', { timeZone: 'Asia/Jakarta' })} WIB`
         }
     }
@@ -15,7 +15,7 @@ export default class GlobalService {
     public static async health(): Promise<GlobalServiceHealthResponse> {
         return {
             'response': true,
-            'message': 'Cihuy, API Kerja Praktik sehat-sehat saja! 😁',
+            'message': 'Cihuy, API Absensi HIMATIF UIN Suska Riau sehat-sehat saja! 😁',
             'status': 'OK',
             'uptime': process.uptime(),
             'memoryUsage': process.memoryUsage()
